f22684abd61a4c6a59b8e696facff1a976db5085
Wes Contreras <wes@wescontreras.com>
2021-05-09 16:47:45 -0700

    trailing slashes

--- app.js
+++ app.js
@@ -19,6 +19,15 @@ const StrategyGoogle = require('passport-google-oauth20').Strategy;
 app.set('views', path.join(__dirname, 'views/pages'));
 app.set('view engine', 'ejs');
 
+// Get rid of trailing slashes
+app.use((req, res, next) => {
+  const test = /\?[^]*\//.test(req.url);
+  if (req.url.substr(-1) === '/' && req.url.length > 1 && !test)
+    res.redirect(301, req.url.slice(0, -1));
+  else
+    next();
+});
+
 // Session storage
 app.use(session({
   store: new (require('connect-pg-simple')(session))(),
